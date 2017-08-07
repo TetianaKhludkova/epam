27f005b646d071f0a48d3cb8bd0e481313cddaba
tetiana <tetiana@idalko.com>
2017-08-07 23:50:00 +0300

    homework2/fibonnachi add method to get number by index

--- src/com/main/java/homework2/Fibonnachi.java
+++ src/com/main/java/homework2/Fibonnachi.java
@@ -9,6 +9,7 @@ public class Fibonnachi {
     public static BigInteger current = BigInteger.valueOf(1);
     public static ArrayList<BigInteger> arrayList = new ArrayList<>();
 
+    public static final Integer INDEX = 5;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
             try {
@@ -18,17 +19,33 @@ public class Fibonnachi {
                     arrayList.add(previous);
                     countFibonnachi(previous, current, count);
                     System.out.println(arrayList);
+                    scanner.close();
                 } else {
                     System.out.println("Enter number more then 1");
+                    scanner.close();
                 }
             } catch (Exception e) {
                 System.out.println("Invalid input. Not a number");
+                scanner.close();
             }
+            getNumberByIndex(INDEX);
+
     }
 
     private static void countFibonnachi(BigInteger previous, BigInteger  current, Integer count){
         if (arrayList.size() == count)  return;
             arrayList.add(current);
             countFibonnachi(current, previous.add(current), count);
+    }
+
+    private static BigInteger getNumberByIndex(Integer INDEX){
+        BigInteger numberByIndex = null;
+        try {
+            numberByIndex = arrayList.get(INDEX);
+            System.out.printf("Number at index %d is %d", INDEX, numberByIndex);
+        } catch (Exception e) {
+            System.out.println("Invalid input. Not a number");
+        }
+        return numberByIndex;
     }
 }
